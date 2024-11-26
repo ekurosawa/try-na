d254648ebd4a447f931496749a62482d58d17947
ekurosawa <mmina0202@gmail.com>
2024-11-26 11:10:22 +0900

    inls

--- components/CallButton.js
+++ components/CallButton.js
@@ -1,9 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
 // components/CallButton.js
 const CallButton = ({ onStartCall, onEndCall }) => {
     return (
       <div>
-        <button onClick={onStartCall}>Start Call</button>
-        <button onClick={onEndCall}>End Call</button>
+        <Button onClick={onStartCall}>Start Call</Button>
+        <Button onClick={onEndCall}>End Call</Button>
       </div>
     );
   };
